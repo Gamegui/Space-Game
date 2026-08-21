fc39b6cc6cf4ed3e44ced62877012f1c49b2ed35
Gamegui <197638913+Gamegui@users.noreply.github.com>
2026-08-21 08:08:44 +0000

    Revert "feat: switch game viewport to native 16:9"
    This reverts commit 670fdc212943725381e2bf7ff8fef6c34acee20d.

--- src/game/gameLoop.ts
+++ src/game/gameLoop.ts
@@ -10,7 +10,7 @@ import { audio } from "./audio";
 import { applyShipClassStats } from "./shipClasses";
 
 export const W = 960;
-export const H = 540;
+export const H = 720;
 let _id = 100000;
 export const uid = () => ++_id;
 
